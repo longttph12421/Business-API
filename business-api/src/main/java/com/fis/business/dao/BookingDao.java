86d6136d21a7edb2729cda2b81117182ba7368d2
Thành Long <59786092+longttph12421@users.noreply.github.com>
2021-11-17 15:11:01 +0700

    update product

--- business-api/src/main/java/com/fis/business/dao/BookingDao.java
+++ business-api/src/main/java/com/fis/business/dao/BookingDao.java
@@ -3,6 +3,9 @@ package com.fis.business.dao;
 import com.fis.business.entity.Booking;
 import com.fis.fw.common.generics.GenericDao;
 
-public interface BookingDao extends GenericDao<Booking,Integer> {
+import java.util.List;
+import java.util.Map;
 
+public interface BookingDao extends GenericDao<Booking,Integer> {
+    List<Booking> saveBooking(Map map);
 }
--- business-api/src/main/java/com/fis/business/entity/Booking.java
+++ business-api/src/main/java/com/fis/business/entity/Booking.java
@@ -14,6 +14,7 @@ public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String status;
 
     @ManyToOne
